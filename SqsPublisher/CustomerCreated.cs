725429ea77b5d80d91cdc91a4a23427e7ddf1a3e
Dan Mehlqvist <dan.mehlqvist@gmail.com>
2023-04-25 12:18:13 +0200

    Built the publisher

---
+++ SqsPublisher/CustomerCreated.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqsPublisher
+{
+    public class CustomerCreated
+    {
+
+
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string GitHubUsername { get; set; }
+
+
+        private CustomerCreated() { }
+
+        public CustomerCreated(string email, string fullName, DateTime dateOfBirth, string gitHubUsername)
+        {
+            Id = Guid.NewGuid();
+            Email = email;
+            FullName = fullName;
+            DateOfBirth = dateOfBirth;
+            GitHubUsername = gitHubUsername;
+        }
+    }
+}
--- SqsPublisher/Program.cs
+++ SqsPublisher/Program.cs
@@ -1,12 +1,45 @@
 ﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Text.Json;
 
 namespace SqsPublisher
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            // https://sqs.eu-north-1.amazonaws.com/742873788565/customers
             AmazonSQSClient client = new AmazonSQSClient();
+
+            CustomerCreated customer = new CustomerCreated(
+                "danbanan@example.com",
+                "Dan Banan",
+                new DateTime(1976, 01, 07),
+                "danbanan"
+                );
+
+            GetQueueUrlResponse amazonUrl = await client.GetQueueUrlAsync("customers");
+
+            var sendMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = amazonUrl.QueueUrl,
+                MessageBody = JsonSerializer.Serialize(customer),
+                MessageAttributes = new Dictionary<string, MessageAttributeValue>
+                {
+                    {
+                        "MessageType", new MessageAttributeValue
+                        {
+                            DataType="string",
+                            StringValue = nameof(CustomerCreated)
+                        }
+                    }
+                }
+            };
+
+            SendMessageResponse response = await client.SendMessageAsync(sendMessageRequest);
+
+            Console.WriteLine($"{response.HttpStatusCode}");
+            Console.ReadKey();
         }
     }
 }
