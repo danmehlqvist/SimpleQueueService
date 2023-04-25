d7e428ecf8ccd4ffcd9d8004a6182155146dc4a5
Dan Mehlqvist <dan.mehlqvist@gmail.com>
2023-04-25 11:44:08 +0200

    Add project files.

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.SQS;
+
+namespace SqsPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AmazonSQSClient client = new AmazonSQSClient();
+        }
+    }
+}
